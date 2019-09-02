cadbbc22da9cd1a84ab8b62472ede857fb437207
Juyoung <juy5790@outlook.com>
2019-09-02 19:59:41 +0900

    first commit

---
+++ src/main/java/com/zoyi/i18nservice/Application.java
@@ -0,0 +1,13 @@
+package com.zoyi.i18nservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
